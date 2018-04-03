e8b0ec93cdb660c06abd8eee0bca1db628a62fff
John Sprunger <johnleesprunger@gmail.com>
2018-04-03 10:55:36 -0400

    Added base controller

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,7 +1,26 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: john.sprunger
- * Date: 4/2/2018
- * Time: 10:23 AM
- */
+/*
+* Base Controller
+* Loads the models and views
+*/
+
+class Controller {
+    // Load model
+    public function model($model){
+        // Require model file
+        require_once '../app/models/' . $model . '.php';
+
+        // Instatiate model
+        return new $model();
+    }
+
+    // Load view
+    public function view($view, $data = []){
+        // Check for view file
+        if (file_exists('../app/views/' . $view . '.php')) {
+            require_once '../app/views/' . $view . '.php';
+        } else {
+            die('View does not exist');
+        }
+    }
+}
