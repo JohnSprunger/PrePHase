c61c95092ce9c00d51d98a3cbc0df3d93a82a71e
John Sprunger <johnleesprunger@gmail.com>
2018-04-04 11:13:46 -0400

    Added Autoloader and Config File

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+
+  // URL Root
+  define('URLROOT', 'http://192.168.33.10/PrePHase/');
+
+  // Site Name
+  define('SITENAME', 'PrePHase');
