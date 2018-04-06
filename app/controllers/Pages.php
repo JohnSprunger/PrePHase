466559bffcac1e31b08eee9e26251dbfcee96caa
John Sprunger <johnleesprunger@gmail.com>
2018-04-06 12:08:02 -0400

    Added Database Class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,15 +1,15 @@
 <?php
   class Pages extends Controller {
     public function __construct(){
-
+        $this->postModel = $this->model('Post');
     }
 
     public function index(){
-      $data = [
-        'title' => 'Welcome'
-      ];
+        $data = [
+            'title' => 'Welcome'
+        ];
 
-      $this->view('pages/index', $data);
+        $this->view('pages/index', $data);
     }
 
     public function about(){
---
+++ app/libraries/Database.php
@@ -0,0 +1,37 @@
+<?php
+class Database{
+    private $host = DB_HOST;
+    private $user = DB_USER;
+    private $pass = DB_PASS;
+    private $dbname = DB_NAME;
+
+    private $dbh;
+    private $stmt;
+    private $error;
+
+    /* An optional constructor for the Database */
+    function constructDB($host, $user, $pass, $dbname){
+        $this->host = $host;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->dbname = $dbname;
+    }
+
+    public function __construct(){
+        // Set DSN
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+
+        // Create PDO instance
+        try{
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch(PDOException $e){
+            $this->error = $e->getMessage();
+            echo $this->error;
+        }
+    }
+
+}
