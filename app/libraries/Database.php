2f1dfbb43539d8c37d70c7be7acac3c29cc1ada2
John Sprunger <johnleesprunger@gmail.com>
2018-04-06 12:22:38 -0400

    Added a query and bind method to db class

--- app/libraries/Database.php
+++ app/libraries/Database.php
@@ -34,4 +34,29 @@ class Database{
         }
     }
 
+    // Prepare statement with query
+    public function query($sql){
+        $this->stmt - $this->dbh->prepare($sql);
+    }
+
+    // Bind Values
+    public function bind($param, $value, $type = null){
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_int($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_int($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+
+        $this->stmt->bindValue($param, $value, $type);
+    }
 }
