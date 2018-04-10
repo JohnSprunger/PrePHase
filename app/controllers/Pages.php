89fb6945ca936ff1fa045e0dc888f7fffbdec5f0
John Sprunger <johnleesprunger@gmail.com>
2018-04-10 08:38:42 -0400

    Finished DB Class

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -5,8 +5,11 @@
     }
 
     public function index(){
+        $posts = $this->postModel->getPosts();
+
         $data = [
-            'title' => 'Welcome'
+            'title' => 'Welcome',
+            'posts' => $posts
         ];
 
         $this->view('pages/index', $data);
